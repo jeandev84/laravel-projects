47212f9555389f43be2bef2d949b3135473a9d28
jeandev84 <jeandev84@yahoo.com>
2022-08-16 11:30:28 +0300

    fix Vue restfulapi

---
+++ blog/README.md
@@ -0,0 +1,9 @@
+1. Installation Vue component for working with Laravel
+```php 
+
+$ composer require laravel/ui:*
+$ php artisan ui vue --auth
+$ npm install && npm run dev
+
+$ php artisan serve
+```
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,19 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', 'SpaController@index')
-    ->where('any', '.*');
-
-
-/*
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-*/
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
