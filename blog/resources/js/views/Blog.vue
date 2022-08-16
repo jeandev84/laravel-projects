72a814be719cbf450faaa67b1d3db2a726e4f7f2
jeandev84 <jeandev84@yahoo.com>
2022-08-16 17:07:15 +0300

    hf templates

--- blog/resources/js/views/Blog.vue
+++ blog/resources/js/views/Blog.vue
@@ -1,27 +1,29 @@
 <template>
-     <div style="display:flex; flex-wrap: wrap;">
+     <div>
          <spin v-if="loading"></spin>
-         <div v-else>
-             <div class="uk-card uk-card-default uk-width-1-2@m">
-                 <div class="uk-grid-small uk-flex-middle" uk-grid>
-                     <div class="uk-width-auto">
-                         <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg">
-                     </div>
-                     <div class="uk-width-expand">
-                         <h3 class="uk-card-title uk-margin-remove-bottom">Title</h3>
-                         <p class="uk-text-meta uk-margin-remove-top">
-                             <time datetime="2016-04-01T19:00">April 19, 2022</time>
-                         </p>
+         <div style="display:flex; flex-wrap: wrap;" v-else>
+             <div class="uk-card uk-card-default uk-width-1-2@m" v-for="post in posts">
+                 <div class="uk-card-header">
+                     <div class="uk-grid-small uk-flex-middle" uk-grid>
+                         <div class="uk-width-auto">
+                             <!-- <img class="uk-border-circle" width="40" height="40" src="images/avatar.jpg">-->
+                         </div>
+                         <div class="uk-width-expand">
+                             <h3 class="uk-card-title uk-margin-remove-bottom">{{ post.title }}</h3>
+                             <p class="uk-text-meta uk-margin-remove-top">
+                                 <time datetime="2016-04-01T19:00">{{ post.created_at }}</time>
+                             </p>
+                         </div>
                      </div>
                  </div>
                  <div class="uk-card-body">
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, nemo?
+                     <p>{{ post.body }}</p>
                  </div>
                  <div class="uk-card-footer">
                      <a href="#" class="uk-button uk-button-text">Read more</a>
                  </div>
-             </div>
-         </div>
+            </div>
+       </div>
      </div>
 </template>
 
