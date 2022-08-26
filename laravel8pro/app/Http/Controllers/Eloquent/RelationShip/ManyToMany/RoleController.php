8ab2cd69b5a8007f60ed199b15d8e124a0781e19
jeandev84 <jeandev84@yahoo.com>
2022-08-26 21:22:33 +0300

    Eloquent RelationShip ManyToMany

--- laravel8pro/app/Http/Controllers/Eloquent/RelationShip/ManyToMany/RoleController.php
+++ laravel8pro/app/Http/Controllers/Eloquent/RelationShip/ManyToMany/RoleController.php
@@ -33,6 +33,7 @@ class RoleController extends Controller
 
       public function addUser()
       {
+          /*
           $user = new User();
           $user->name = "John";
           $user->email = "john@gmail.com";
@@ -42,6 +43,18 @@ class RoleController extends Controller
           // 1: ID Administrator
           // 2: ID Editor
           $roleIds = [1, 2];
+          */
+
+          $user = new User();
+          $user->name = "Brown";
+          $user->email = "brown@gmail.com";
+          $user->password = encrypt('secret');
+          $user->save();
+
+          // 2: ID Editor
+          // 3: ID Author
+          // 4: ID Contributor
+          $roleIds = [2, 3, 4];
 
           $user->roles()->attach($roleIds);
 
